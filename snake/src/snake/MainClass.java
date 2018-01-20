9414fd6e5002462f078030f054185fa0d7698345
大脚肥牛 <athenazhz@126.com>
2018-01-20 14:44:22 +0800

    snake
    this is my first project

---
+++ snake/src/snake/MainClass.java
@@ -0,0 +1,37 @@
+package snake;
+import java.awt.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class MainClass extends JFrame {
+	 ControlSnake control;
+	
+	 Toolkit kit;
+	
+	 Dimension dimen;
+
+ public static void main(String[] args) {
+	 new MainClass("By ¥ÛΩ≈∑ ≈£");
+ }
+
+ public MainClass(String s) {
+	 super(s);
+	 control = new ControlSnake();
+	 control.setFocusable(true);
+	 kit = Toolkit.getDefaultToolkit();
+	 dimen = kit.getScreenSize();
+
+	 add(control);
+	 setLayout(new BorderLayout());
+	 setLocation(dimen.width / 3, dimen.height / 3);
+	 setSize(FWIDTH, FHEIGHT);
+	 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 setResizable(false);
+	 setVisible(true);
+ }
+
+ public static final int FWIDTH = 315;
+
+ public static final int FHEIGHT = 380;
+}
+
